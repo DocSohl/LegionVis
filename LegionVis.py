6ea98e2a617ef033972352b446c9df0c8ebf8348
DocSohl <docsohl@gmail.com>
2015-11-09 16:06:38 -0700

    Add basic analyzer, web server and test data

---
+++ LegionVis.py
@@ -0,0 +1,29 @@
+
+from BaseHTTPServer import HTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler 
+from shutil import copyfileobj
+
+class Handler(SimpleHTTPRequestHandler):
+    def json_out(self, obj):
+        self.wfile.write(json.dumps(obj)+"\n")
+    def do_POST(self):
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],}
+        )
+        print form
+    def do_GET(self):
+	STATICS = ('/Data','/Interface')
+        if self.path == "/":
+            self.send_response(200, "OKAY")
+            self.end_headers()
+            copyfileobj(open('Interface/display.html', 'r'),self.wfile)
+	elif reduce(lambda a, b: a or b, (self.path.startswith(k) for k in STATICS)):
+		SimpleHTTPRequestHandler.do_GET(self)
+            
+if __name__=="__main__":
+    server_address = ('', 8001)
+    httpd = HTTPServer(server_address, Handler)
+    httpd.serve_forever()
---
+++ ProcessData.py
@@ -0,0 +1,32 @@
+from __future__ import division
+import math
+import re
+
+prefix = r'\[(?P<node>[0-9]+) - (?P<thread>[0-9a-f]+)\] \{\w+\}\{legion_prof\}: '
+
+
+def analyze(data):
+    tasks = []
+    for line in data:
+        match = re.compile(prefix + r'Prof Task Info (?P<tid>[0-9]+) (?P<fid>[0-9]+) (?P<pid>[a-f0-9]+) (?P<create>[0-9]+) (?P<ready>[0-9]+) (?P<start>[0-9]+) (?P<stop>[0-9]+)').match(line)
+        if match is not None:
+            tasks.append({
+                "task_id" : long(match.group('tid')),
+                "func_id" : int(match.group('fid')),
+                "proc_id" : int(match.group('pid'),16),
+                "create" : long(match.group('create'))/1000,
+                "ready" : long(match.group('ready'))/1000,
+                "start" : long(match.group('start'))/1000,
+                "stop" : long(match.group('stop'))/1000
+            })
+    return tasks
+
+
+
+
+if __name__=="__main__":
+    # Load the file
+    test = open("data/PROF.log","r")
+    testdata = test.readlines()
+    result = analyze(testdata)
+    print result
