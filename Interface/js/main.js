9339f16837141d7e46341ed61a9d78f83e081349
Phil <u0764757@utah.edu>
2015-11-30 13:08:41 -0700

    Finished mainview.js refactoring

--- Interface/js/main.js
+++ Interface/js/main.js
@@ -1,5 +1,5 @@
 
-var svg, xAxis, taskcontainer, zoom, width, height, names, timedata, color, histview;
+var svg, xAxis, taskcontainer, zoom, width, height, names, timedata, color, histview, mainview;
 
 /**
  * Scan the time series task data and modify it with concurrency numbers
@@ -46,236 +46,21 @@ function Init(){
         timedata = data[0]; // An array of task objects
         names = data[1]; // A map of function IDs to task names
         var maxStacks = scanData(timedata); // Scan through the data to find concurrencies
-        var margin = {top: 20, right: 20, bottom: 30, left: 120};
-        width = 1200 - margin.left - margin.right; // TODO: These values should be fixed to adjust to screen size
-        height = 800 - margin.top - margin.bottom;
+
+        mainview = new MainView(timedata, names, maxStacks);
+        mainview.update();
+
         histview = new HistogramView(timedata);
         d3.select("#histcount").on("change",function(){
-            console.log("clicked Count!");
             histview.update("Count");
         });
         d3.select("#histtime").on("change",function(){
-            console.log("clicked Time!");
             histview.update("Time");
         });
-
-        var x = d3.scale.linear().domain([0,d3.max(timedata,function(d){return d.stop;})]).range([0,width]);
-        var y = d3.scale.ordinal().domain(timedata.map(function(d){return d.proc_id;})).rangeRoundBands([0,height],0.1);
-
-        zoom = d3.behavior.zoom() // Zoom only on the x dimension
-            .scaleExtent([1,40])// TODO: These should be adjusted by total time
-            .x(x)
-            .on("zoom", zoomed);
-
-        svg = d3.select("#timeline") // The main view
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")").call(zoom); // Zoom on entire view
-
-        svg.append("rect") // Set up an invisible screen that allows zooming anywhere
-            .attr("width",width)
-            .attr("height",height)
-            .style("fill","none")
-            .style("pointer-events","all");
-
-        /*D3 tip code from http://bl.ocks.org/Caged/6476579*/
-        var tip = d3.tip() // Set up tooltips on hover
-            .attr('class', 'd3-tip')
-            .offset([-10, 0])
-            .html(function(d) {
-                return "<strong>Func_ID:</strong> <span style='color:red'>" + names[d.func_id] + "</span>";
-            });
-        svg.call(tip);
-
-        var funcs = d3.set(timedata.map(function(d){return d.func_id;})).values(); // A list of function IDs
-        var stacks = d3.set(timedata.map(function(d){return d.stack;})).values(); // A list of possible concurrencies
-        var procs = d3.set(timedata.map(function(d){return d.proc_id;})).values(); // A list of processor IDs
-
-        color = d3.scale.ordinal().domain(funcs)// Manually defined colors. TODO: make automatic
-            .range(['#a6cee3','#b2df8a','#fb9a99','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928']);
-
-        xAxis = d3.svg.axis().scale(x).orient("bottom");
-
-        var y1s = {}; // Map of processors to d3 ordinal scales
-        for(var i = 0; i < procs.length; ++i){
-            y1s[procs[i]] = d3.scale.ordinal();
-            y1s[procs[i]].domain(stacks.slice(0,maxStacks[procs[i]])).rangeRoundBands([y.rangeBand(),0]);
-        }
-
-        svg.append("g")
-            .attr("class","x axis")
-            .attr("transform","translate(0,"+height+")")
-            .call(xAxis);
-
-
-        taskcontainer = svg.append("g"); // Main view container
-
-        var clip = svg.append("defs").append("svg:clipPath") // Set up a clip path so tasks don't appear offscreen
-            .attr("id", "clip")
-            .append("svg:rect")
-            .attr("id", "clip-rect")
-            .attr("x", "0")
-            .attr("y", "0")
-            .attr("width", width)
-            .attr("height", height);
-
-        var tasks = taskcontainer.selectAll(".task") // Set up each processor list
-            .data(procs)
-            .enter().append("g")
-            .attr("class","g")
-            .attr("transform",function(d){return "translate(0,"+y(d)+")";}); // Put each proc in its own g
-
-
-        tasks.append("rect") // A box to indicate the range of the processor
-            .attr("class","processor")
-            .attr("x",-1)
-            .attr("y",-1)
-            .attr("width",width+2)
-            .attr("height",y.rangeBand()+2); // Extra padding to allow a border
-
-
-        tasks.append("text") // Name of the processor
-            .attr("class","proc-label")
-            .style("text-anchor", "end")
-            .attr("x",-10)
-            .attr("y",y.rangeBand()/2)
-            .text(function(d){return "Processor #"+ d;});
-
-        var clipg = tasks.append("g") // Limit the actual task boxes by the clipping window
-            .attr("clip-path","url(#clip)");
-
-        var subtasks = clipg.append("g").attr("class","subtasks");
-
-
-        subtasks.selectAll(".task") // Add the actual task boxes
-            .data(function(d){return timedata.filter(function(e){return d == e.proc_id;})}) // Limit to just this proc
-            .enter().append("rect").attr("class","task")
-            .attr("height",function(d){return y1s[d.proc_id].rangeBand();}) // Use local scale
-            .attr("y",function(d){return y1s[d.proc_id](d.stack);}) // Scale based on concurrency
-            .attr("x",function(d){return x(d.start);})
-            .attr("width",function(d){return x(d.stop - d.start);}) // This is associative
-            .style("fill",function(d){return color(d.func_id);})
-            .on('mouseover', tip.show)
-            .on('mouseout', tip.hide)
-            .on('mouseup',taskClicked);
-
-        var svglegend = d3.select("#legend") // Set up the legend in a separate SVG
-            .attr("width",400)
-            .attr("height",20*funcs.length+30);
-
-        svglegend.append("text").attr("y",20).attr("x",50).text("Task Name"); // Legend title
-
-        var legend = svglegend.selectAll(".legend") // Generate legend based on entries
-            .data(funcs.slice())
-            .enter().append("g")
-            .attr("class", "legend")
-            .attr("transform", function(d, i) { return "translate(50," + ((i+1) * 20 + 10) + ")"; });
-
-        legend.append("rect") // Box to indicate color
-            .attr("x", 0)
-            .attr("width", 18)
-            .attr("height", 18)
-            .style("fill", color);
-
-        legend.append("text")
-            .attr("x", 24)
-            .attr("y", 9)
-            .attr("dy", ".35em")
-            .style("text-anchor", "start")
-            .text(function(d) { return names[d]; });
-
 
         histview.update("Count"); // Set up the histogram
 
     });
-}
-
-/**
- * First run populates the histogram chart, and subsequent runs updates the histogram
- */
-function updateHist(checkedOption){
-    var bins = []; // List of bin names
-    var binned = {}; // Map of bins to values
-    var maxval = 0; // Largest bin (for y scale)
-    timedata.forEach(function(d){ //Iterate over every element in the task series
-        var val = d.func_id; // Everything is stored via function ID
-        if(!binned[val]){ // Add to the map if it doesn't exist
-            binned[val] = 0;
-            bins.push(val); // Register name
-        }
-        if(checkedOption == "Count") // If we're just counting the number of occurrences
-            binned[val]++;
-        else{ // Otherwise base on run time
-            binned[val] += d.stop - d.start;
-        }
-        maxval = Math.max(maxval,binned[val]); // Keep track of largest value
-    });
-
-    var margin = {top: 20, right: 30, bottom: 30, left: 60};
-    if(maxval>100000){ // TODO: Make scaling proportional to number size
-        margin.left = 80;
-    }
-    var histwidth = 500 - margin.left - margin.right;
-    var histheight = 500 - margin.top - margin.bottom;
-
-
-    var x = d3.scale.ordinal().rangeRoundBands([0,histwidth],0.1)
-        .domain(bins.map(function(d){return names[d];}));
-    var y = d3.scale.linear().range([histheight,0])
-        .domain([0,maxval]);
-
-    var histxAxis = d3.svg.axis().scale(x).orient("bottom");
-    var histyAxis = d3.svg.axis().scale(y).orient("left");
-
-    var chart;
-    if(histFirst){ // For the first iteration, set everything up
-        chart = d3.select("#hist")
-            .attr("width",histwidth + margin.left + margin.right)
-            .attr("height",histheight + margin.top + margin.bottom)
-            .append("g").attr("class","contents").attr("transform","translate(" + margin.left + "," + margin.top + ")");
-
-        chart.append("g").attr("class","x axis")
-            .attr("transform","translate(0," + histheight + ")")
-            .call(histxAxis);
-
-        chart.append("g").attr("class","y axis")
-            .call(histyAxis);
-
-        chart.append("text").attr("class","yLabel") // Y axis label
-            .attr("transform","rotate(-90)")
-            .attr("y",-margin.left)
-            .attr("x",-histheight/2)
-            .attr("dy","1em")
-            .style("text-anchor","middle")
-            .text("Count");
-
-        histFirst = false; // Next time just update
-    }
-    else{ // Otherwise update existing values
-        chart = d3.select("#hist").select(".contents");
-        chart.transition() // Smoothly move between scales
-            .attr("transform","translate(" + margin.left + "," + margin.top + ")");
-        chart.select(".x.axis").transition().call(histxAxis);
-        chart.select(".y.axis").transition().call(histyAxis);
-        if(checkedOption == "Count") { // Change the label too
-            chart.select(".yLabel").transition().attr("y",-margin.left).text("Count");
-        }
-        else{ // These have to transition as well so they don't pop out
-            chart.select(".yLabel").transition().attr("y",-margin.left).text("Total Time (\u03BCs)");
-        }
-    }
-
-    var bars = chart.selectAll(".bar").data(bins); // The actual data boxes
-
-    bars.enter().append("rect").attr("class","bar");
-
-    bars.transition() // Smoothly move between states
-        .attr("x",function(d){return x(names[d]);})
-        .attr("y",function(d){return y(binned[d]);})
-        .attr("height",function(d){return histheight - y(binned[d]);})
-        .attr("width", x.rangeBand())
-        .style("fill",function(d){return color(d);});
 }
 
 /**
---
+++ Interface/js/mainview.js
@@ -0,0 +1,143 @@
+/**
+ * Created by Phil on 11/30/2015.
+ */
+function MainView(_timedata, _names, _maxStacks){
+    var self = this;
+
+    self.timedata = _timedata;
+    self.names = _names;
+    self.maxStacks = _maxStacks;
+}
+
+MainView.prototype.update = function(){
+    var self = this;
+    var margin = {top: 20, right: 20, bottom: 30, left: 120};
+    width = 1200 - margin.left - margin.right; // TODO: These values should be fixed to adjust to screen size
+    height = 800 - margin.top - margin.bottom;
+    var x = d3.scale.linear().domain([0,d3.max(self.timedata,function(d){return d.stop;})]).range([0,width]);
+    var y = d3.scale.ordinal().domain(self.timedata.map(function(d){return d.proc_id;})).rangeRoundBands([0,height],0.1);
+
+    zoom = d3.behavior.zoom() // Zoom only on the x dimension
+        .scaleExtent([1,40])// TODO: These should be adjusted by total time
+        .x(x)
+        .on("zoom", zoomed);
+
+    svg = d3.select("#timeline") // The main view
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")").call(zoom); // Zoom on entire view
+
+    svg.append("rect") // Set up an invisible screen that allows zooming anywhere
+        .attr("width",width)
+        .attr("height",height)
+        .style("fill","none")
+        .style("pointer-events","all");
+
+    /*D3 tip code from http://bl.ocks.org/Caged/6476579*/
+    var tip = d3.tip() // Set up tooltips on hover
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+            return "<strong>Func_ID:</strong> <span style='color:red'>" + self.names[d.func_id] + "</span>";
+        });
+    svg.call(tip);
+
+    var funcs = d3.set(self.timedata.map(function(d){return d.func_id;})).values(); // A list of function IDs
+    var stacks = d3.set(self.timedata.map(function(d){return d.stack;})).values(); // A list of possible concurrencies
+    var procs = d3.set(self.timedata.map(function(d){return d.proc_id;})).values(); // A list of processor IDs
+
+    color = d3.scale.ordinal().domain(funcs)// Manually defined colors. TODO: make automatic
+        .range(['#a6cee3','#b2df8a','#fb9a99','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928']);
+
+    xAxis = d3.svg.axis().scale(x).orient("bottom");
+
+    var y1s = {}; // Map of processors to d3 ordinal scales
+    for(var i = 0; i < procs.length; ++i){
+        y1s[procs[i]] = d3.scale.ordinal();
+        y1s[procs[i]].domain(stacks.slice(0,self.maxStacks[procs[i]])).rangeRoundBands([y.rangeBand(),0]);
+    }
+
+    svg.append("g")
+        .attr("class","x axis")
+        .attr("transform","translate(0,"+height+")")
+        .call(xAxis);
+
+
+    taskcontainer = svg.append("g"); // Main view container
+
+    var clip = svg.append("defs").append("svg:clipPath") // Set up a clip path so tasks don't appear offscreen
+        .attr("id", "clip")
+        .append("svg:rect")
+        .attr("id", "clip-rect")
+        .attr("x", "0")
+        .attr("y", "0")
+        .attr("width", width)
+        .attr("height", height);
+
+    var tasks = taskcontainer.selectAll(".task") // Set up each processor list
+        .data(procs)
+        .enter().append("g")
+        .attr("class","g")
+        .attr("transform",function(d){return "translate(0,"+y(d)+")";}); // Put each proc in its own g
+
+
+    tasks.append("rect") // A box to indicate the range of the processor
+        .attr("class","processor")
+        .attr("x",-1)
+        .attr("y",-1)
+        .attr("width",width+2)
+        .attr("height",y.rangeBand()+2); // Extra padding to allow a border
+
+
+    tasks.append("text") // Name of the processor
+        .attr("class","proc-label")
+        .style("text-anchor", "end")
+        .attr("x",-10)
+        .attr("y",y.rangeBand()/2)
+        .text(function(d){return "Processor #"+ d;});
+
+    var clipg = tasks.append("g") // Limit the actual task boxes by the clipping window
+        .attr("clip-path","url(#clip)");
+
+    var subtasks = clipg.append("g").attr("class","subtasks");
+
+
+    subtasks.selectAll(".task") // Add the actual task boxes
+        .data(function(d){return self.timedata.filter(function(e){return d == e.proc_id;})}) // Limit to just this proc
+        .enter().append("rect").attr("class","task")
+        .attr("height",function(d){return y1s[d.proc_id].rangeBand();}) // Use local scale
+        .attr("y",function(d){return y1s[d.proc_id](d.stack);}) // Scale based on concurrency
+        .attr("x",function(d){return x(d.start);})
+        .attr("width",function(d){return x(d.stop - d.start);}) // This is associative
+        .style("fill",function(d){return color(d.func_id);})
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide)
+        .on('mouseup',taskClicked);
+
+    var svglegend = d3.select("#legend") // Set up the legend in a separate SVG
+        .attr("width",400)
+        .attr("height",20*funcs.length+30);
+
+    svglegend.append("text").attr("y",20).attr("x",50).text("Task Name"); // Legend title
+
+    var legend = svglegend.selectAll(".legend") // Generate legend based on entries
+        .data(funcs.slice())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(50," + ((i+1) * 20 + 10) + ")"; });
+
+    legend.append("rect") // Box to indicate color
+        .attr("x", 0)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+
+    legend.append("text")
+        .attr("x", 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "start")
+        .text(function(d) { return self.names[d]; });
+
+}
